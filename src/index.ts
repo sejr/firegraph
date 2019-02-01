0d1226f398dd2cefbfdb0a36a08a1a8b66e57965
Sam Jackson <sam@clique.app>
2019-02-01 09:11:14 -0500

    docs: doc comments for selectDocument

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,14 @@ type GraphQLSelectionSet = {
     selections?: GraphQLSelection[];
 }
 
+/**
+ * Retrieves a single document from a specified document path. Retrieves
+ * all fields indicated in the GraphQL selection set, including any nested
+ * collections or references that are defined.
+ * @param store An initialized Firestore instance.
+ * @param documentPath The path of the document we want to retrieve.
+ * @param selectionSet The rules for defining the documents we want to get.
+ */
 async function selectDocument(
     store: firebase.firestore.Firestore,
     documentPath: string,
